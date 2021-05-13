a9a753b909ce66b4becf83b6b86cc0262a43555c
martingotz <gotzm@udesa.edu.ar>
2021-05-13 17:36:30 -0300

    controlador

--- controller/controlador.js
+++ controller/controlador.js
@@ -1,20 +1,35 @@
+const db = require('../database/models')
+const producto = db.Producto;
+const op = db.Sequelize.Op;
+
 let controlador = {
 
     index:  (req, res) =>{ // Es la pagina que se va a ver cuando el usuario busque home
-        res.render('index')
+        productos.findAll()
+            .then((resultados)=> res.render('productos'),{resultados})
+            .catch((err)=> `Error: ${err}`)
     },
     product:  (req, res) =>{
         res.render('product')
     },
     productAdd:  (req, res) =>{ 
-        res.render('product-add')
+        return res.render('product-add')
     },
     productEdit:  (req, res) =>{ 
         res.render('product-edit')
     },
     
     searchResults:  (req, res) =>{ 
-        res.render('search-results')
+        let searchResults = req.query.search
+        productos.findAll({
+            where: [
+                {title:{[op.like]:`%${searchResults}%` }}
+            ]
+        })
+        .then((resultados)=> res.render('searchResults'),{resultados})
+        .catch((err)=> `Error: ${err}`)
+
+        //res.render('search-results')
     },
 
 }
