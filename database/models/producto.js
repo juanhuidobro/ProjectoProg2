51d2c11a0cd53299f8de2ed850a1d90b9a2f3a82
martingotz <gotzm@udesa.edu.ar>
2021-05-24 13:22:24 -0300

    modelo

---
+++ database/models/producto.js
@@ -0,0 +1,41 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias= "Productos";
+    let cols={
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER,
+        },
+        marca:{
+            allowNull: false,
+            type: dataTypes.STRING,
+        },
+        modelo:{
+            allowNull: false,
+            type: dataTypes.STRING,
+        },
+        imagen:{
+            allowNull: false,
+            type: dataTypes.STRING,
+        },
+        descripcion:{
+            allowNull: false,
+            type: dataTypes.STRING,
+        },
+        precio:{
+            allowNull: false,
+            type: dataTypes.STRING,
+        },
+        email:{
+            allowNull: false,
+            type: dataTypes.STRING,//Chekear no se si esta bien
+        },
+        
+};
+let config={
+    tableName:"productos",
+    timeStamps: false,
+};
+const Producto = sequelize.define(alias,cols,config);
+return Producto;
+}
