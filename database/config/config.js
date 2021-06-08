76a8c1ad2add55ea08bc36210622e4f51eeccbe1
juanhuidobro <57154635+juanhuidobro@users.noreply.github.com>
2021-06-08 16:34:49 -0300

    merge de comits

--- database/config/config.js
+++ database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "root",
+    "password": null,
     "database": "proyecto_parcial",
     "host": "127.0.0.1",
     "dialect": "mysql"
